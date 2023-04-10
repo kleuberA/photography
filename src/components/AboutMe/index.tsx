db3093f4018616fbe1ff5cb1cc94de2d87ab3160
Kleuber Silva <kleuber20@hotmail.com>
2023-04-09 23:25:07 -0300

    New AboutMe

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -1,29 +1,34 @@
 import './style.css';
+import ImageAboutMe from '../../assets/imageaboutme.jpeg';
 
 export default function AboutMe(){
     return(
-        <div className="w-full h-[100vh] bg-[#E1FEFE] flex justify-center items-center">
-            <div className='w-[80%] h-[65vh]'>
-                <div className="grid grid-rows-1 w-full grid-flow-col auto-cols-max">
-                    <div className="imageAboutMe col-span-1 row-span-1 h-[486px] w-[445px]"></div>
-                    <div className='col-span-2 row-span-1 w-[770px] bg-white h-[486px]'>
-                        <div className='pl-6 flex flex-col h-[486px] w-full gap-7 items-start justify-center'>
-                            <span className='text-[#828282] text-lg font-medium tracking-[5px] leading-5'>Photographer</span>
-                            <h1 className='text-[#333333] text-4xl font-medium tracking-[5px] leading-5'>Anna Jennifer </h1>
-                            <p className='w-[80%] text-justify text-[#828282] tracking-[5px] leading-5 font-normal'>Consectetur elit consectetur culpa laborum commodo cillum duis voluptate aliquip. 
-                                Cillumm pariatur id duis cupidatat non eu duis voluptate commodo ut. 
-                                Do cupidatat ipsum ex velit cillum consectetur cupidatat amet. 
-                                Veniam occaecat est anim sint exercitation adipisicing. 
-                                Sunt occaecat commodo quis commodo culpa.
-                            </p>
-                            <button className=' border-[#333333] transition-all font-semibold border-2 p-3 text-[#333333] hover:bg-[#333333] hover:text-white'>Learn More</button>
+        <div className="bg-[#E1FEFE] w-full h-[100vh] flex justify-center items-center">  
+            <div className="m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div className="mb-12 space-y-2 text-center"></div>
+                <div className="grid gap-12 lg:grid-cols-2 lg:grid-rows-2">
+                    <div className="p-1 col-span-2 h-96 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+                        <img src={ImageAboutMe} alt="art cover" loading="lazy" width="1000" height="667" className="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl"/>
+                        <div className="sm:w-7/12 pl-0 p-5">
+                            <div className="space-y-2">
+                                <div className="space-y-4">
+                                    <h4 className="text-lg text-[#828282] font-medium tracking-[5px] leading-5">Photographer</h4>
+                                    <h1 className="text-xl text-[#333333] font-medium tracking-[5px] leading-5">Anna Jennifer</h1>
+                                    <p className="text-gray-600">
+                                        Consectetur elit consectetur culpa laborum commodo cillum duis voluptate aliquip. 
+                                        Cillumm pariatur id duis cupidatat non eu duis voluptate commodo ut. 
+                                        Do cupidatat ipsum ex velit cillum consectetur cupidatat amet. 
+                                        Veniam occaecat est anim sint exercitation adipisicing. 
+                                        Sunt occaecat commodo quis commodo culpa.
+                                    </p>
+                                </div>
+                                <button className="block w-max border-[#333333] transition-all font-semibold border-2 p-3 text-[#333333] hover:bg-[#333333] hover:text-white">Read more</button>
+                            </div>
                         </div>
                     </div>
+
                 </div>
             </div>
         </div>
     )
 }
-
-// width: 445px;
-// height: 602px;
